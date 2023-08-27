feb188c9a48a9d832b563eb2ceaed481528018f6
Bandiu <Bandiubandi@gmail.com>
2023-08-27 23:21:59 +0300

    add line of concatenate base path and file path

--- src/main/java/servlets/FileDownloadServlet.java
+++ src/main/java/servlets/FileDownloadServlet.java
@@ -6,23 +6,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLDecoder;
 
-
+@WebServlet("/downloadFile")
 public class FileDownloadServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String filePath = request.getParameter("path");
-        File file = new File(filePath);
+        String filePath = URLDecoder.decode(request.getParameter("path"),"UTF-8");
+        String filePathFull = HtmlContentMaker.getRoot()+"/"+filePath;
+        File file = new File(filePathFull);
 
         response.setContentType("application/octet-stream");
+        System.out.println(filePathFull);
         response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
 
         try (InputStream inputStream = new FileInputStream(file);
              OutputStream outputStream = response.getOutputStream()) {
 
+            System.out.println("Start download");
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
+
             }
         }
     }
